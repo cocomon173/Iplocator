49da339ba79badd6028afc086e1f60df5273085d
cocomon173 <cocomon173@gmail.com>
2020-06-27 15:56:53 +0900

    iplocator.pl

---
+++ iplocator.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+# Ip Geolocation 
+#By : Lucifer
+# Demo : https://www.youtube.com/watch?v=0Eq3d10LQ8c
+#
+# API documentation - http://ip-api.com/docs/
+# you can see you external ip using this Bash script :
+# read COUNTRY IP <<< $(wget -qO- http://ip-api.com/line/?fields=countryCode,query); echo $IP $COUNTRY
+# Now let's start :
+
+use Socket;
+use Term::ANSIColor;
+use WWW::Mechanize;
+use JSON;
+  
+ 
+print color 'bold bright_yellow';
+ 
+print q{
+
+   00000  00000  00000  00000    0   0    00000  0    0  1 77777 33333
+   0      0   0  0      0   0   0 0 0 0   0   0  0 0  0  1    7  ====3
+   0      0   0  0      0   0  0   0   0  0   0  0  0 0  1   7       3
+   00000  00000  00000  00000  0   0   0  00000  0    0  1   7   33333
+
+Ip Geolocation Tool  
+By : Cocomon173
+
+----------------------------------------------------------------------
+};
+ 
+print color 'reset';
+@iphost=$ARGV[0] || die "Usage : perl iplocation.pl [host] [ip] [domain] \n\nEx:  perl iplocation.pl  www.google.com \n     ./iplocation.pl  216.58.210.206\n \n";
+my @ip = inet_ntoa(scalar gethostbyname("@iphost")or die "IP or Host invalid!\n");
+my @hn = scalar gethostbyaddr(inet_aton(@ip),AF_INET);
+ 
+my $GET=WWW::Mechanize->new();
+    $GET->get("http://ip-api.com/json/@ip"); # JSON API OF IP-API.COM
+    my $json = $GET->content();
+ 
+ 
+my $info = decode_json($json);
+# Json API Response :
+# A successful request will return, by default, the following:
+#{
+#    "status": "success",
+#    "country": "COUNTRY",
+#    "countryCode": "COUNTRY CODE",
+#    "region": "REGION CODE",
+#    "regionName": "REGION NAME",
+#    "city": "CITY",
+#    "zip": "ZIP CODE",
+#    "lat": LATITUDE,
+#    "lon": LONGITUDE,
+#    "timezone": "TIME ZONE",
+#    "isp": "ISP NAME",
+#    "org": "ORGANIZATION NAME",
+#    "as": "AS NUMBER / NAME",
+#   "query": "IP ADDRESS USED FOR QUERY"
+# }
+# INFOS OF JSON API ...
+ 
+print "  [!] IP: ", $info->{'query'}, "\n";
+print "------------------------------------\n"; 
+print "  [+] ORG: ", $info->{'as'}, "\n";
+print "  [+] ISP: ", $info->{'isp'}, "\n";
+print "  [+] Country: ", $info->{'country'}," - ", $info->{'countryCode'}, "\n";
+print "  [+] City: ", $info->{'city'}, "\n";
+print "  [+] Region: ", $info->{'regionName'}, " - " , $info->{'region'}, "\n";
+print "  [+] Geo: ", "Lat: " , $info->{'lat'}, " - Long: ", $info->{'lon'}, "\n";
+print "  [+] Geo: ", "Latitude: " , $info->{'lat'}, " - Long: ", $info->{'lat'}, "\n";
+print "  [+] Time: ", "timezone: " , $info->{'timezone'}, " - Long: ", $info->{'timezone'}, "\n";
+print "  [+] As number/name: ", "as: " , $info->{'as'}, " - Long: ", $info->{'as'}, "\n";
+print "  [+] ORG: ", $info->{'as'}, "\n";
+print "  [+] Country code: ", $info->{'countryCode'}, "\n"; 
+print "  [+] Status: ", $info->{'status'}, "\n"; 
+print "\n";
+# EOF
